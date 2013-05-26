b9227f13a4b0c57a34e97fcd9e2fa4d87a712a50
Bibby Chung/n <bibbynet@gmail.com>
2013-05-27 01:05:25 +0800

    Init

---
+++ MvcApplication1/Controllers/TestController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication1.Controllers
+{
+    public class TestController : Controller
+    {
+        //
+        // GET: /Test/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Demo1() { return View(); }
+        public ActionResult Demo2() { return View(); }
+        public ActionResult Demo3() { return View(); }
+        public ActionResult Demo4() { return View(); }
+        public ActionResult Demo5() { return View(); }
+        public ActionResult Demo6() { return View(); }
+
+
+        public ActionResult Final()
+        {
+            return View();
+        }
+
+    }
+}
